0b51740153ad956da513342c5924b9860b5f5a95
dwictor0 <daniel.wdev0@gmail.com>
2026-04-13 16:10:49 -0300

    FEAT: definindo funcoes make_salt e derive_key

---
+++ index.py
@@ -0,0 +1,19 @@
+import pathlib,os,secrets,base64,getpass
+import cryptography
+from cryptography . fernet import Fernet
+from cryptography . hazmat . primitives .kdf . scrypt import Scrypt
+
+def make_salt(size=16):
+    """_summary_
+    Args:
+        size (int, optional): _description_. Defaults to 16.
+    """
+    return secrets.token_bytes(size)
+def derive_key(salt,password):
+    """_summary_
+    Args:
+        salt (_type_): _description_
+        password (_type_): _description_
+    """
+    kdf = Scrypt(salt=salt,length=32,n = 2 ** 14,r = 8,p=1)
+    return kdf.derive(password.encode())
